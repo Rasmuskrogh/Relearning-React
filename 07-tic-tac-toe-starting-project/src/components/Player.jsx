0f75a0142f28123aa71fe1486de8c0182e79911e
[Rasmus Krogh-Andersen] <[rasmus.kroghandersen@gmail.com]>
2024-04-16 19:29:28 +0200

    added the player component and the functionality of changing player name, added the GameBoard component and started making functionality for putting down X or O

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const Player = ({ initialName, symbol }) => {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing((editing) => !editing);
+  };
+
+  const handleChange = (e) => {
+    console.log(e, e.target.value);
+    setPlayerName(e.target.value);
+  };
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+};
+
+export default Player;
